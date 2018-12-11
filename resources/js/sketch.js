7ff82f2c050845e11cf8d5bcf99e6d301c8aec74
Iván Fernández <ifernandez@rutatrol.com>
2018-12-11 10:41:22 -0300

    Draw code was added.

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -7,6 +7,7 @@ function setup() {
 		profileBox = select('#profile-div');
 
 	select('#hide-profile-button').mouseClicked(() => {
+		background(55, 105, 75);
 		allowDrawing = true;
 		navbarButton.show();
 		profileBox.hide();
@@ -15,20 +16,46 @@ function setup() {
 	});
 
 	navbarButton.mouseClicked(() => {
+		background(255, 255, 255);
 		allowDrawing = false;
 		navbarButton.hide();
 		profileBox.show();
-		background(255);
 		footer.show();
 		noLoop();
 	});
 
 	canvas.style('z-index', '-1');
 	canvas.position(0, 0);
+
+	rectMode(CENTER);
+	smooth();
 }
 
 function draw() {
 	if (allowDrawing) {
-		// Drawing code here!
+		translate(width / 2, height / 2);
+
+		if (!mouseIsPressed) {
+			let size = constrain(mouseY / 3, 10, mouseY / 3),
+				r = random(256),
+				g = random(256),
+				b = random(256),
+				alpha = random(256);
+
+			// center square
+			push();
+			noStroke();
+			fill(color(r, g, b, alpha));
+			rotate(radians(frameCount));
+			rect(0, 0, size, size);
+			pop();
+
+			// Other squares
+			if (frameCount % 5 === 0) {
+				fill(color(255 - r, 255 - g, 255 - b, alpha));
+			}
+			rect(-width/2 + mouseX, -height/2 + mouseY, size, size);
+			rect(width/2 - mouseX, height/2 - mouseY, size, size);
+		}
 	}
 }
