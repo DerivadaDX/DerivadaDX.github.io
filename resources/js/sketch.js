a2443d59c45cbf1525805454f8e5444f7aace639
Iván Fernández <ifernandez@rutatrol.com>
2018-12-11 10:34:26 -0300

    Flow control drawing. Footer is displayed or hidden like the profile div.

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -1,18 +1,34 @@
+let allowDrawing = false;
+
 function setup() {
-	let navbarButton = select('#hide-navbar-button'),
+	let canvas = createCanvas(windowWidth, windowHeight),
+		navbarButton = select('#hide-navbar-button'),
+		footer = select('#derivadadx-footer'),
 		profileBox = select('#profile-div');
 
 	select('#hide-profile-button').mouseClicked(() => {
-		navbarButton.show(); profileBox.hide();
+		allowDrawing = true;
+		navbarButton.show();
+		profileBox.hide();
+		footer.hide();
+		loop();
 	});
 
 	navbarButton.mouseClicked(() => {
+		allowDrawing = false;
 		navbarButton.hide();
 		profileBox.show();
+		background(255);
+		footer.show();
+		noLoop();
 	});
 
+	canvas.style('z-index', '-1');
+	canvas.position(0, 0);
 }
 
 function draw() {
-	// some code here
+	if (allowDrawing) {
+		// Drawing code here!
+	}
 }
