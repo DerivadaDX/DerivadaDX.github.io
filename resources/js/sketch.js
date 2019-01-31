38efe3c309b71057fc0b21800b65d1fe77add73a
Iván Fernández <ifernandez@rutatrol.com>
2019-01-31 09:46:02 -0300

    Botón click me muestra sketch en div sketch-holder

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -1,61 +1,52 @@
 let allowDrawing = false;
 
 function setup() {
-	let canvas = createCanvas(windowWidth, windowHeight),
-		navbarButton = select('#hide-navbar-button'),
-		footer = select('#derivadadx-footer'),
-		profileBox = select('#profile-div');
+	let sketchHolder = select('#sketch-holder'),
+		switchButton = select('#switch-sketch-button'),
+		canvas = createCanvas(sketchHolder.width - 2, sketchHolder.height - 2);
 
-	select('#hide-profile-button').mouseClicked(() => {
-		background(55, 105, 75);
-		allowDrawing = true;
-		navbarButton.show();
-		profileBox.hide();
-		footer.hide();
-		loop();
+	canvas.parent('sketch-holder');
+	canvas.hide();
+
+	switchButton.mouseClicked(() => {
+		if (!switchButton.isPressed) {
+			canvas.show();
+			background(55, 105, 75);
+			switchButton.isPressed = true;
+		} else {
+			canvas.hide();
+			switchButton.isPressed = false;
+		}
 	});
 
-	navbarButton.mouseClicked(() => {
-		background(255, 255, 255);
-		allowDrawing = false;
-		navbarButton.hide();
-		profileBox.show();
-		footer.show();
-		noLoop();
-	});
-
-	canvas.style('z-index', '-1');
 	canvas.position(0, 0);
-
 	rectMode(CENTER);
 	smooth();
 }
 
 function draw() {
-	if (allowDrawing) {
-		translate(width / 2, height / 2);
+	translate(width / 2, height / 2);
 
-		if (!mouseIsPressed) {
-			let size = constrain(mouseY / 3, 10, mouseY / 3),
-				r = random(256),
-				g = random(256),
-				b = random(256),
-				alpha = random(256);
+	if (!mouseIsPressed) {
+		let size = constrain(mouseY / 3, 10, mouseY / 3),
+			r = random(256),
+			g = random(256),
+			b = random(256),
+			alpha = random(256);
 
-			// center square
-			push();
-			noStroke();
-			fill(color(r, g, b, alpha));
-			rotate(radians(frameCount));
-			rect(0, 0, size, size);
-			pop();
+		// center square
+		push();
+		noStroke();
+		fill(color(r, g, b, alpha));
+		rotate(radians(frameCount));
+		rect(0, 0, size, size);
+		pop();
 
-			// Other squares
-			if (frameCount % 5 === 0) {
-				fill(color(255 - r, 255 - g, 255 - b, alpha));
-			}
-			rect(-width/2 + mouseX, -height/2 + mouseY, size, size);
-			rect(width/2 - mouseX, height/2 - mouseY, size, size);
+		// Other squares
+		if (frameCount % 5 === 0) {
+			fill(color(255 - r, 255 - g, 255 - b, alpha));
 		}
+		rect(-width / 2 + mouseX, -height / 2 + mouseY, size, size);
+		rect(width / 2 - mouseX, height / 2 - mouseY, size, size);
 	}
 }
