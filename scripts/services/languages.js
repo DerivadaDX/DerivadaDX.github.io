edd80831829fef11b082a755303ca862be255238
DerivadaDX <derivadadx@gmail.com>
2020-02-23 01:47:40 -0300

    [T-8] Se implementó LanguageService.

---
+++ scripts/services/languages.js
@@ -0,0 +1,23 @@
+class LanguageService {
+	constructor() {
+		this.lng = localStorage.getItem('lang');
+
+		if (!this.lng) {
+			this.lng = 'en'; // default
+			localStorage.setItem('lang', this.lng);
+		}
+	}
+
+	initialize() {
+		let scriptTag = document.createElement('script');
+
+		scriptTag.type = 'text/javascript';
+		scriptTag.src = `../../locale/${this.lng}.js`;
+
+		document.querySelector('head').appendChild(scriptTag);
+
+		scriptTag.onload = () => {
+			console.log(`${this.lng}.js file loaded`);
+		};
+	}
+}
