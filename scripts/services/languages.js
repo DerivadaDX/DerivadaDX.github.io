2da09f33be50c1aa4d808f3fabb23be20672a42e
DerivadaDX <derivadadx@gmail.com>
2020-02-23 02:11:09 -0300

    [T-8] Se agregó setLanguage a LanguageService.

--- scripts/services/languages.js
+++ scripts/services/languages.js
@@ -1,21 +1,31 @@
 class LanguageService {
 	constructor() {
-		this.lng = localStorage.getItem('lang');
-
-		if (!this.lng) {
-			this.lng = 'en'; // default
-			localStorage.setItem('lang', this.lng);
-		}
+		this._saveLanguage(localStorage.getItem('lang') || 'en');
 	}
 
 	initialize() {
 		let scriptTag = document.createElement('script');
 
+		scriptTag.id = 'script-lang';
 		scriptTag.type = 'text/javascript';
 		scriptTag.src = `../../locale/${this.lng}.js`;
 
 		document.querySelector('head').appendChild(scriptTag);
 
 		scriptTag.onload = () => document.lang.forEach(e => $(`.${e.id}`).html(e.text));
+	}
+
+	setLanguage(lng) {
+		let scriptTag = document.getElementById('script-lang');
+
+		scriptTag.parentNode.removeChild(scriptTag);
+
+		this._saveLanguage(lng);
+		this.initialize();
+	}
+
+	_saveLanguage(lng) {
+		this.lng = lng;
+		localStorage.setItem('lang', this.lng);
 	}
 }
